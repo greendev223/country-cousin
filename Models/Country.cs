2b11c7c3759aecd337eebce0e987461df813247f
Lauren McCall <lmmccolloch@gmail.com>
2022-05-19 12:55:49 -0400

    created Country model and DatabaseContext

---
+++ Models/Country.cs
@@ -0,0 +1,14 @@
+namespace CountryCuisine.Models
+{
+    public class Country
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string PhotoUrl { get; set; }
+        public string FlagUrl { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeUrl { get; set; }
+        public string MovieTitle { get; set; }
+        public string Musician { get; set; }
+    }
+}
--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,7 +14,7 @@ namespace CountryCuisine.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
+        public DbSet<Country> Countries { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
