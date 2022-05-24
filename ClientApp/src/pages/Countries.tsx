c4e80b3f16cf07bb0b7562fdc71707fce11ca044
Lauren McCall <lmmccolloch@gmail.com>
2022-05-24 16:08:11 -0400

    search function on Countries page working

--- ClientApp/src/pages/Countries.tsx
+++ ClientApp/src/pages/Countries.tsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 
 import { CountryType /*CSSStarsProperties*/ } from '../types'
 import { SingleCountryFromList } from '../components/SingleCountryFromList'
 
 export function Countries() {
+  const [filterText, setFilterText] = useState('')
+
   const { data: countries = [] } = useQuery<CountryType[]>(
-    'countries',
+    ['countries', filterText],
     async function () {
-      const response = await fetch('/api/countries')
+      let url = '/api/countries'
+
+      if (filterText.length !== 0) {
+        url = `/api/countries?filter=${filterText}`
+      }
+
+      const response = await fetch(url)
       return response.json()
     }
   )
-  console.log({ countries })
 
   return (
     <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+          onChange={function (event) {
+            setFilterText(event.target.value)
+          }}
+        />
+      </form>
       <article>
         {countries
           .sort((a, b) => (a.name > b.name ? 1 : 0))
